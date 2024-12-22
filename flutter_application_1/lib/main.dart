539210a96e3ba5bd3aec4685a2dda0d3a9943111
luongdangxt <160085709+luongdangxt@users.noreply.github.com>
2024-12-23 00:18:02 +0700

    a

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -40,98 +40,97 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: Size(392.72, 856.72), // Kích thước thiết kế ban đầu
+      designSize: const Size(392.72, 856.72), // Kích thước thiết kế ban đầu
       builder: (context, child) {
         return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        title: Text(
-          titles[indexScreen],
-          style: const TextStyle(
-            fontWeight: FontWeight.bold
-          ),   
-        ),
-        actions: [
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.local_fire_department, color: Colors.grey,size: 35.h),
-                onPressed: () {},
-              ),
-              Text(
-                '0',
-                style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 18.sp,
-                ),
-              ),
-              Stack(
+          appBar: AppBar(
+            centerTitle: false,
+            title: Text(
+              titles[indexScreen],
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            actions: [
+              Row(
                 children: [
                   IconButton(
-                    icon: Icon(Icons.notifications, color: Colors.black,size: 35.h),
+                    icon: Icon(Icons.local_fire_department,
+                        color: Colors.grey, size: 35.h),
                     onPressed: () {},
                   ),
-                  Positioned(
-                    right: 7.w,
-                    top: 7.h,
-                    child: CircleAvatar(
-                      radius: 10.r,
-                      backgroundColor: Colors.red,
-                      child: Text(
-                        '2',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 13.h,
+                  Text(
+                    '0',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18.sp,
+                    ),
+                  ),
+                  Stack(
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.notifications,
+                            color: Colors.black, size: 35.h),
+                        onPressed: () {},
+                      ),
+                      Positioned(
+                        right: 7.w,
+                        top: 7.h,
+                        child: CircleAvatar(
+                          radius: 10.r,
+                          backgroundColor: Colors.red,
+                          child: Text(
+                            '2',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13.h,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.settings, color: Colors.black),
+                    onPressed: () {},
                   ),
                 ],
               ),
-              IconButton(
-                icon: const Icon(Icons.settings, color: Colors.black),
-                onPressed: () {},
+            ],
+          ),
+          body: screens[indexScreen],
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            fixedColor: const Color.fromARGB(255, 32, 164, 36),
+            currentIndex: indexScreen,
+            onTap: (e) {
+              setState(() {
+                indexScreen = e;
+              });
+            },
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.calendarCheck),
+                label: 'Today',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.gamepad),
+                label: 'Trò chơi',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.question_mark),
+                label: 'câu đố',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(FontAwesomeIcons.clipboardList),
+                label: 'Bài kiểm tra',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Hồ sơ',
               ),
             ],
           ),
-        ],
-      ),
-      body: screens[indexScreen],
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed, 
-        fixedColor: const Color.fromARGB(255, 32, 164, 36),
-        currentIndex: indexScreen,
-        onTap: (e) {
-          setState(() {
-            indexScreen = e;
-          });
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(FontAwesomeIcons.calendarCheck),
-            label: 'Today',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FontAwesomeIcons.gamepad),
-            label: 'Trò chơi',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.question_mark),
-            label: 'câu đố',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(FontAwesomeIcons.clipboardList),
-            label: 'Bài kiểm tra',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Hồ sơ',
-          ),
-        ],
-      ),
-    );
+        );
       },
     );
   }
 }
-
