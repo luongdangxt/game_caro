2a4e8b528af4308e55ae2dbddb125dc453bd7512
nduong11 <nongduong0811@gmail.com>
2025-01-22 23:07:15 +0700

    casc

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/setup_sence.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_application_1/UI/forgotpass.dart';
 import 'package:flutter_application_1/UI/loading.dart';
 import 'package:flutter_application_1/UI/register.dart';
 import 'package:flutter_application_1/request/apiUser.dart';
 import 'package:flutter_application_1/request/saveLogin.dart';
-import 'package:flutter_application_1/setup_sence.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    routes: {
-      '/login': (context) => const loginScreen(),
-      '/register': (context) => registerScreen(),
-    },
-    home: const loginScreen(),
+  runApp(ScreenUtilInit(
+    designSize: const Size(390, 844), // Kích thước thiết kế ban đầu
+    builder: (context, child) => MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/login': (context) => const loginScreen(),
+        '/register': (context) => registerScreen(),
+      },
+      home: const loginScreen(),
+    ),
   ));
 }
 
@@ -48,6 +52,7 @@ class _loginScreenState extends State<loginScreen> {
 
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,8 +68,8 @@ class _loginScreenState extends State<loginScreen> {
             ),
           ),
           Container(
-            height: 500,
-            width: 500,
+            height: 400.h,
+            width: 400.w,
             decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('assets/images/khung_login.png'),
@@ -72,188 +77,178 @@ class _loginScreenState extends State<loginScreen> {
               ),
             ),
             child: Center(
-              // Center widget bao bọc toàn bộ Column
               child: Column(
-                mainAxisAlignment:
-                    MainAxisAlignment.center, // Căn giữa theo trục dọc
-                crossAxisAlignment:
-                    CrossAxisAlignment.center, // Căn giữa theo trục ngang
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 23, bottom: 20.0),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.h, bottom: 5.h),
                     child: Text(
                       'Gomoku Online',
                       style: TextStyle(
-                        fontSize: 27,
+                        fontSize: 27.sp,
                         fontWeight: FontWeight.bold,
                         color: Colors.white,
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 1.0, bottom: 16, left: 50, right: 50),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          TextField(
-                            controller: usernameController,
-                            decoration: const InputDecoration(
-                              labelText: 'Username',
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.person),
-                              filled: true, // Bật tính năng nền màu
-                              fillColor: Color.fromARGB(
-                                  255, 255, 255, 255), // Chọn màu nền bạn muốn
-                            ),
-                            keyboardType: TextInputType.emailAddress,
+                  SizedBox(height: 30.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50.w),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        TextField(
+                          controller: usernameController,
+                          decoration: const InputDecoration(
+                            labelText: 'Username',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.person),
+                            filled: true,
+                            fillColor: Colors.white,
                           ),
-                          const SizedBox(height: 16),
-                          TextField(
-                            controller: passwordController,
-                            decoration: const InputDecoration(
-                              labelText: 'Password',
-                              border: OutlineInputBorder(),
-                              prefixIcon: Icon(Icons.lock),
-                              filled: true, // Bật tính năng nền màu
-                              fillColor: Color.fromARGB(
-                                  255, 255, 255, 255), // Chọn màu nền bạn muốn
-                            ),
-                            obscureText: true,
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        SizedBox(height: 16.h),
+                        TextField(
+                          controller: passwordController,
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            border: OutlineInputBorder(),
+                            prefixIcon: Icon(Icons.lock),
+                            filled: true,
+                            fillColor: Colors.white,
                           ),
-                          const SizedBox(height: 24),
-                          ElevatedButton(
-                              onPressed: () async {
-                                final username = usernameController.text;
-                                final password = passwordController.text;
+                          obscureText: true,
+                        ),
+                        SizedBox(height: 24.h),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final username = usernameController.text;
+                            final password = passwordController.text;
 
-                                if (username.isNotEmpty &&
-                                    password.isNotEmpty) {
-                                  try {
-                                    // Đặt isLoading thành true khi bắt đầu quá trình đăng nhập
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => const myLoading(
-                                              isLoading: true,
-                                              errorMessage: null,
-                                              backScreen: '/login')),
-                                    );
-                                    final response = await DataUser()
-                                        .login(username, password);
-                                    if (response == 200) {
-                                      final prefs =
-                                          await SharedPreferences.getInstance();
-                                      await prefs.setString(
-                                          'username', username);
-                                      Navigator.pushAndRemoveUntil(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const HomeScreen()),
-                                        (Route<dynamic> route) => false,
-                                      );
-                                    } else {
-                                      // Cập nhật trạng thái khi đăng nhập không thành công
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => const myLoading(
-                                            isLoading: true,
-                                            errorMessage:
-                                                'Invalid username or password',
-                                            backScreen: '/login',
-                                          ),
-                                        ),
-                                      );
-                                    }
-                                  } catch (e) {
-                                    // Cập nhật trạng thái khi xảy ra lỗi
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) => const myLoading(
-                                            isLoading: true,
-                                            errorMessage: 'Login failed',
-                                            backScreen: '/login'),
-                                      ),
-                                    );
-                                  }
+                            if (username.isNotEmpty && password.isNotEmpty) {
+                              try {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const myLoading(
+                                          isLoading: true,
+                                          errorMessage: null,
+                                          backScreen: '/login')),
+                                );
+                                final response = await DataUser().login(
+                                  username,
+                                  password,
+                                );
+                                if (response == 200) {
+                                  final prefs =
+                                      await SharedPreferences.getInstance();
+                                  await prefs.setString('username', username);
+                                  Navigator.pushAndRemoveUntil(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const HomeScreen(),
+                                    ),
+                                    (Route<dynamic> route) => false,
+                                  );
                                 } else {
-                                  // Khi không có tên người dùng hoặc mật khẩu
                                   Navigator.push(
                                     context,
                                     MaterialPageRoute(
                                       builder: (context) => const myLoading(
-                                          isLoading: true,
-                                          errorMessage:
-                                              'Please enter username and password!',
-                                          backScreen: '/login'),
+                                        isLoading: true,
+                                        errorMessage:
+                                            'Invalid username or password',
+                                        backScreen: '/login',
+                                      ),
                                     ),
                                   );
                                 }
-                              },
-                              child: const Padding(
-                                padding: EdgeInsets.only(top: 12, bottom: 12),
-                                child: Text(
-                                  'Sign - in',
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 0, 0, 0),
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              )),
-                          const SizedBox(height: 8),
-                          SizedBox(
-                            width: 300,
-                            height: 50,
-                            child: TextButton(
-                                onPressed: () {
-                                  Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => registerScreen(),
+                              } catch (e) {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const myLoading(
+                                      isLoading: true,
+                                      errorMessage: 'Login failed',
+                                      backScreen: '/login',
                                     ),
-                                  );
-                                },
-                                child: const Text(
-                                  'Sign - up',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
                                   ),
-                                )),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pushReplacement(
+                                );
+                              }
+                            } else {
+                              Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => forgotpassScreen(),
+                                  builder: (context) => const myLoading(
+                                    isLoading: true,
+                                    errorMessage:
+                                        'Please enter username and password!',
+                                    backScreen: '/login',
+                                  ),
                                 ),
                               );
-                            },
-                            child: const Text(
-                              'Forgot Password?',
+                            }
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 12.h),
+                            child: Text(
+                              'Sign - in',
                               style: TextStyle(
-                                color: Color.fromARGB(255, 207, 207, 207),
-                                fontSize: 12,
+                                color: Colors.black,
+                                fontSize: 18.sp,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(height: 8.h),
+                        SizedBox(
+                          width: 300.w,
+                          height: 50.h,
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => registerScreen(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Sign - up',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => forgotpassScreen(),
+                              ),
+                            );
+                          },
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: const Color(0xFFCFCFCF),
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  SizedBox(height: 20.h),
                 ],
               ),
             ),
